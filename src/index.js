d9e57e5059cf3f016421c71aab834b4a3c425d96
Руслан Королев <techfusionwork@gmail.com>
2019-03-11 21:43:31 +0300

    make paddle move

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 (function () {
-    // Inputs, Commands
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
+    document.addEventListener('keydown', keyDownHandler, null);
+    document.addEventListener('keyup', keyUpHandler, null);
     let dy = 4;
     let dx = 4;
 
@@ -10,8 +11,11 @@
         ballRadius: 10,
         x: 10,
         y: 10,
+        paddleX: 0,
         paddleHeight: 10,
-        paddleWidth: 100
+        paddleWidth: 100,
+        rightPressed: false,
+        leftPressed: false
     };
 
     function update() {
@@ -22,6 +26,14 @@
 
         if (game.y + dy > canvas.height - game.ballRadius || game.y + dy < game.ballRadius) {
             dy = -dy;
+        }
+
+        if (game.rightPressed) {
+            game.paddleX += 8;
+        }
+
+        if (game.leftPressed) {
+            game.paddleX -= 8;
         }
         game.x += dx;
         game.y += dy;
@@ -42,10 +54,28 @@
 
     function drawThePaddle() {
         ctx.beginPath();
-        ctx.rect((canvas.width - game.paddleWidth) / 2, canvas.height - game.paddleHeight, game.paddleWidth, game.paddleHeight);
+        ctx.rect(game.paddleX, canvas.height - game.paddleHeight, game.paddleWidth, game.paddleHeight);
         ctx.fillStyle = "#0095DD";
         ctx.fill();
         ctx.closePath();
+    }
+
+    function keyDownHandler(e) {
+        if (e.key === 'ArrowRight') {
+            game.rightPressed = true;
+        }
+        if (e.key === 'ArrowLeft') {
+            game.leftPressed = true;
+        }
+    }
+
+    function keyUpHandler(e) {
+        if (e.key === 'ArrowRight') {
+            game.rightPressed = false;
+        }
+        if (e.key === 'ArrowLeft') {
+            game.leftPressed = false;
+        }
     }
 
     function main(tFrame) {
