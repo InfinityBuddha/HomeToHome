5ec7903bad28b5e1f19bde47f9b72f52488537ac
Руслан Королев <techfusionwork@gmail.com>
2019-03-11 21:25:18 +0300

    draw paddle

--- src/index.js
+++ src/index.js
@@ -2,34 +2,58 @@
     // Inputs, Commands
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
+    let dy = 4;
+    let dx = 4;
 
-    let myGame = {
-        counter: 0,
+    let game = {
         stopMain: null,
-        x: 0,
-        y: 0
+        ballRadius: 10,
+        x: 10,
+        y: 10,
+        paddleHeight: 10,
+        paddleWidth: 100
     };
 
-    function update (tFrame) {
-        myGame.stopMain = tFrame;
-        myGame.x += 1;
-        myGame.y += 1;
+    function update() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        if (game.x + dx > canvas.width - game.ballRadius || game.x + dx < game.ballRadius) {
+            dx = -dx;
+        }
+
+        if (game.y + dy > canvas.height - game.ballRadius || game.y + dy < game.ballRadius) {
+            dy = -dy;
+        }
+        game.x += dx;
+        game.y += dy;
     }
 
-    function render () {
+    function render() {
+        drawTheBall();
+        drawThePaddle();
+    }
+
+    function drawTheBall() {
         ctx.beginPath();
-        ctx.arc(myGame.x, myGame.y, 10, 0, Math.PI*2);
+        ctx.arc(game.x, game.y, game.ballRadius, 0, Math.PI * 2);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    function drawThePaddle() {
+        ctx.beginPath();
+        ctx.rect((canvas.width - game.paddleWidth) / 2, canvas.height - game.paddleHeight, game.paddleWidth, game.paddleHeight);
         ctx.fillStyle = "#0095DD";
         ctx.fill();
         ctx.closePath();
     }
 
     function main(tFrame) {
-        myGame.stopMain = window.requestAnimationFrame(main);
+        game.stopMain = window.requestAnimationFrame(main);
         // main loop content
         update(tFrame);
         render()
-    };
+    }
     main(); // start the cycle
 })();
 
